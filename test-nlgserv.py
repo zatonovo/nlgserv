9e6e9f3baa6904f4d938e522ac52f8147fabb0e0
Darren Richardson <mnestis@users.noreply.github.com>
2014-10-06 18:46:26 +0100

    Fixed test-nlgserv.py so that the nlgserv process is successfully killed.
    Previously, it was being allowed to continue in the background.

--- test-nlgserv.py
+++ test-nlgserv.py
@@ -5,6 +5,7 @@ import subprocess
 from time import sleep
 import urllib2
 import json
+import os
 
 def send_test_data(json_data):
     req = urllib2.Request("http://localhost:8080/generateSentence",
@@ -37,9 +38,15 @@ class TestTense(unittest.TestCase):
         self.assertEqual(send_test_data(json.dumps({"sentence":sentence})), "John will kick Dave.")
 
 if __name__=="__main__":
-    print "Loading nlgserv..."
-    nlgserv = subprocess.Popen(["./nlgserv.py"], stdout=open("/dev/null", "w"), stderr=open("/dev/null", "w"))    
+    print "Starting up nlgserv..."
+    nlgserv = subprocess.Popen(["./nlgserv.py"],
+                               stdin=subprocess.PIPE,
+                               stdout=open(os.path.devnull, "w"),
+                               stderr=open(os.path.devnull, "w"),
+                               preexec_fn=os.setsid)
     sleep(10)
     print "Commencing testing..."
-    unittest.main()
-    nlgserv.kill()
+    unittest.main(exit=False)
+    print "Shutting down nlgserv..."
+    os.killpg(nlgserv.pid, subprocess.signal.SIGTERM)
+    nlgserv.wait()
