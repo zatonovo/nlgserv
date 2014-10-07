cae161d780a96a55789c1b1e6b68e9692b258419
Darren Richardson <mnestis@users.noreply.github.com>
2014-10-07 17:26:52 +0100

    Made the testing script compatible with nosetests and updated config files
    Should now work with TravisCI

--- nlgserv/tests/server-test.py
+++ nlgserv/tests/server-test.py
@@ -7,37 +7,10 @@ import urllib2
 import json
 import os
 
-def send_test_data(json_data):
-    req = urllib2.Request("http://localhost:8080/generateSentence",
-                          data=json_data,
-                          headers={"Content-Type":"application/json"})
-    return urllib2.urlopen(req).read()
+nlgserv = None
 
-class TestCake(unittest.TestCase):
-    def test(self):
-        self.assertEqual(send_test_data(open(os.path.join(os.path.dirname(__file__), "fixtures/cake.json"),"r").read()), "John baked a cake from some ingredients.")
-        
-class TestIngredients(unittest.TestCase):
-    def test(self):
-        self.assertEqual(send_test_data(open(os.path.join(os.path.dirname(__file__), "fixtures/ingredients.json"),"r").read()), "The ingredients were flour, sugar, butter and eggs.")
-
-class TestTense(unittest.TestCase):
-    def test(self):
-        sentence = {}
-        sentence["subject"] = "John"
-        sentence["verb"] = "kick"
-        sentence["object"] = "Dave"
-
-        sentence["features"] = [{"tense":"past"}]
-        self.assertEqual(send_test_data(json.dumps({"sentence":sentence})), "John kicked Dave.")
-
-        sentence["features"] = [{"tense":"present"}]
-        self.assertEqual(send_test_data(json.dumps({"sentence":sentence})), "John kicks Dave.")
-        
-        sentence["features"] = [{"tense":"future"}]
-        self.assertEqual(send_test_data(json.dumps({"sentence":sentence})), "John will kick Dave.")
-
-if __name__=="__main__":
+def setUpModule():
+    global nlgserv
     print __file__
     print "Starting up nlgserv..."
     nlgserv = subprocess.Popen([os.path.join(os.path.dirname(__file__),"../jython.jar"), os.path.join(os.path.dirname(__file__), "../server.py")],
@@ -47,7 +20,40 @@ if __name__=="__main__":
                                preexec_fn=os.setsid)
     sleep(20) # It needs longer now it's loading from the standalone package...
     print "Commencing testing..."
-    unittest.main(exit=False)
+    
+def tearDownModule():
+    global nlgserv
     print "Shutting down nlgserv..."
     os.killpg(nlgserv.pid, subprocess.signal.SIGTERM)
     nlgserv.wait()
+    
+
+def send_data(json_data):
+    req = urllib2.Request("http://localhost:8080/generateSentence",
+                          data=json_data,
+                          headers={"Content-Type":"application/json"})
+    return urllib2.urlopen(req).read()
+
+class TestCake(unittest.TestCase):
+    def test(self):
+        self.assertEqual(send_data(open(os.path.join(os.path.dirname(__file__), "fixtures/cake.json"),"r").read()), "John baked a cake from some ingredients.")
+        
+class TestIngredients(unittest.TestCase):
+    def test(self):
+        self.assertEqual(send_data(open(os.path.join(os.path.dirname(__file__), "fixtures/ingredients.json"),"r").read()), "The ingredients were flour, sugar, butter and eggs.")
+
+class TestTense(unittest.TestCase):
+    def test(self):
+        sentence = {}
+        sentence["subject"] = "John"
+        sentence["verb"] = "kick"
+        sentence["object"] = "Dave"
+
+        sentence["features"] = [{"tense":"past"}]
+        self.assertEqual(send_data(json.dumps({"sentence":sentence})), "John kicked Dave.")
+
+        sentence["features"] = [{"tense":"present"}]
+        self.assertEqual(send_data(json.dumps({"sentence":sentence})), "John kicks Dave.")
+        
+        sentence["features"] = [{"tense":"future"}]
+        self.assertEqual(send_data(json.dumps({"sentence":sentence})), "John will kick Dave.")
