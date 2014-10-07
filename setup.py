697681ec20c6b679f266b6168cdb2220ca62c28d
Darren Richardson <mnestis@users.noreply.github.com>
2014-10-07 17:15:16 +0100

    More preparations for becoming a module.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="NLGServ",
+    version="0.0.0",
+    author="Darren Richardson",
+    packages=["nlgserv","nlgserv.tests"],
+    package_data={"nlgserv":["simplenlg.jar","jython.jar"]}
+    scripts=[],
+    license="LICENSE.txt",
+    description="JSON HTTP wrapper for SimpleNLG",
+    long_description=open("README.md").read()
+)
