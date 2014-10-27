2118d179cbf98791caecaffaa9cc38070a8a11cf
Darren Richardson <mnestis@users.noreply.github.com>
2014-10-27 15:02:51 +0000

    v0.2.0, 2014-10-27 -- API Change: Expressing features 	Features no longer need to be nested in lists, instead use a single dictionary. 	Several other minor features added.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nlgserv",
-    version="0.1.0",
+    version="0.2.0",
     author="Darren Richardson",
     packages=["nlgserv","nlgserv.tests"],
     package_data={"nlgserv":["simplenlg.jar","jython.jar"]},
