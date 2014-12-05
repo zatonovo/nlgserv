e4500b6bcc7f7d3f62714e7323c00e109c108b44
Darren Richardson <mnestis@users.noreply.github.com>
2014-12-05 12:50:19 +0000

    v0.2.2, 2014-12-05 -- Added clauses 	This should allow for the generation of more expressive sentences.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nlgserv",
-    version="0.2.1",
+    version="0.2.2",
     author="Darren Richardson",
     packages=["nlgserv","nlgserv.tests"],
     package_data={"nlgserv":["simplenlg.jar","jython.jar"]},
